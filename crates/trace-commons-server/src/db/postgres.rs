4c3a653e92f4d1cbe5af935a0d5096f8c93836b3
Zaki <zaki@iqlusion.io>
2026-07-12 13:37:19 +0100

    Add dedup columns to trace_gate_decisions (V40) and wire into run_migrations

--- crates/trace-commons-server/src/db/postgres.rs
+++ crates/trace-commons-server/src/db/postgres.rs
@@ -1173,6 +1173,24 @@ impl Database for PgBackend {
                 )
                 .await?;
         }
+        let already_applied = client
+            .query_opt(
+                "SELECT 1 FROM _trace_commons_migrations WHERE version = $1",
+                &[&40_i32],
+            )
+            .await?
+            .is_some();
+        if !already_applied {
+            client
+                .batch_execute(include_str!("../../../../migrations/V40__trace_dedup.sql"))
+                .await?;
+            client
+                .execute(
+                    "INSERT INTO _trace_commons_migrations (version, name) VALUES ($1, $2)",
+                    &[&40_i32, &"trace_dedup"],
+                )
+                .await?;
+        }
         Ok(())
     }
 
